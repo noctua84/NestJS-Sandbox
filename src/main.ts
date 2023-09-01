d52160f63b2b2fa733065897a1fae74fe2b7284a
Markus Möller <markus.moeller@outlook.de>
2023-09-01 09:10:44 +0200

    fix: added a fix for a code style issue shown by qodana
    a promis chain missed a .then()

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
-bootstrap();
+bootstrap().then(() => {
+  console.log('NestJS server started');
+});
