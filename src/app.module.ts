3a29c8994f82cb6ab212675437d9c5b21306f9ed
Markus Möller <markus.moeller@outlook.de>
2023-09-07 23:21:33 +0200

    chore(indentation): switched indentation from two spaces to 4

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HealthController } from './health/health.controller';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [HealthModule],
+    controllers: [AppController, HealthController],
+    providers: [AppService],
 })
 export class AppModule {}
