de970fb491379bc0fbf5f456a2c906e5551760c3
Markus Möller <markus.moeller@outlook.de>
2023-09-09 23:53:30 +0200

    test: updated healt controller test suite

--- src/health/health.controller.spec.ts
+++ src/health/health.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HealthController } from './health.controller';
-import { TerminusModule } from '@nestjs/terminus';
+import { HealthIndicatorResult, TerminusModule } from '@nestjs/terminus';
 import { HttpModule } from '@nestjs/axios';
 
 describe('HealthController', () => {
@@ -17,5 +17,25 @@ describe('HealthController', () => {
 
     it('should be defined', () => {
         expect(controller).toBeDefined();
+    });
+
+    it('should have a health check response with details of all indicators when there are no indicators to check', async () => {
+        const result: HealthIndicatorResult = {
+            info: {
+                status: 'up',
+            },
+            error: {
+                status: 'up',
+            },
+        };
+
+        jest.spyOn(controller['http'], 'pingCheck').mockResolvedValueOnce(
+            result,
+        );
+        const response = await controller.check();
+
+        expect(response.status).toBe('ok');
+        expect(response.info).toBeDefined();
+        expect(response.error).toBeDefined();
     });
 });
