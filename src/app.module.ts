8f0a79622cd512ecce2ec2c64a244520637957d0
Markus Möller <markus.moeller@outlook.de>
2023-09-09 17:48:04 +0200

    feat: integration of nestjs dev tools

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { HealthController } from './health/health.controller';
 import { HealthModule } from './health/health.module';
+import { DevtoolsModule } from '@nestjs/devtools-integration';
 
 @Module({
-    imports: [HealthModule],
-    controllers: [AppController, HealthController],
+    imports: [
+        DevtoolsModule.register({
+            http: process.env.NODE_ENV !== 'production',
+        }),
+        HealthModule,
+    ],
+    controllers: [AppController],
     providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, PartialGraphHost } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { writeFileSync } from 'fs';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule, {
+        snapshot: true,
+        abortOnError: false,
+    });
     await app.listen(3000);
 }
-bootstrap().then(() => {
-    console.log('NestJS server started');
-});
+bootstrap()
+    .then(() => {
+        console.log('NestJS server started');
+    })
+    .catch((err) => {
+        console.error(err);
+        writeFileSync('graph.json', PartialGraphHost.toString() ?? '');
+        process.exit(1);
+    });
