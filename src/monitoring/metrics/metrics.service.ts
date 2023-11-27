bd31dce943f02f41c6d36dce6555e9e3803c76d5
Markus Möller <markus.moeller@outlook.de>
2023-11-27 09:29:07 +0100

    refactor(metrics): changed the way metrics are collected and processed
    the endpoint will now always return default metrics. prisma metrics are only added if a connection
    is possibel. otherwise, an error is logged to the console but without shutting down the app.

--- src/monitoring/metrics/metrics.service.ts
+++ src/monitoring/metrics/metrics.service.ts
@@ -36,17 +36,16 @@ export class MetricsService implements IMetricsService {
      * @returns Promise<string>
      */
     async getMetrics(): Promise<string> {
-        try {
-            this.metricsCollection = [
-                await this.register.metrics(),
-                await this.prisma.$metrics.prometheus(),
-            ];
+        this.metricsCollection.push(await this.register.metrics());
 
-            return this.metricsCollection.join('\n');
+        try {
+            const prismaMetrics = await this.prisma.$metrics.prometheus();
+            this.metricsCollection.push(prismaMetrics);
         } catch (error) {
             console.log('Error retrieving metrics:', error);
-            return '';
         }
+
+        return this.metricsCollection.join('\n');
     }
 
     /**
