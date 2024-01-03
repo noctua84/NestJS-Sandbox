da36ddfac7da003928c1716ef1e2e999bea26410
Markus Möller <markus.moeller@outlook.de>
2024-01-03 23:11:24 +0100

    feat: prisma service methods: addpaging
    a service method has been added to provide paging query params to be added to potential db queries.

--- src/prisma/prisma.service.spec.ts
+++ src/prisma/prisma.service.spec.ts
@@ -8,7 +8,7 @@ describe('PrismaService', () => {
     let mockPrismaClient: MockContext['prisma'];
 
     beforeEach(async () => {
-        const mockContext = createMockContext();
+        const mockContext: MockContext = createMockContext();
         mockPrismaClient = mockContext.prisma;
 
         const module: TestingModule = await Test.createTestingModule({
@@ -38,6 +38,42 @@ describe('PrismaService', () => {
         it('should call $disconnect on application shutdown', async () => {
             await service.onApplicationShutdown();
             expect(mockPrismaClient.$disconnect).toHaveBeenCalledTimes(1);
+        });
+    });
+
+    describe('Service Methods', () => {
+        describe('getPrismaClient', () => {
+            it('should return an instance of the PrismaClient class', () => {
+                const prismaClient: PrismaClient = service.getPrismaClient();
+                expect(prismaClient).toBe(mockPrismaClient);
+            });
+        });
+
+        describe('addPaging', () => {
+            it('should return an empty object when no paging arguments are provided', () => {
+                const pagingArgs = service.addPaging();
+                expect(pagingArgs).toEqual({});
+            });
+
+            it('should return a paging object with cursor arguments when paging type is cursor', () => {
+                process.env.PAGING_TYPE = 'cursor';
+                const pagingArgs = service.addPaging(1, 10);
+                expect(pagingArgs).toEqual({
+                    cursor: {
+                        id: 1,
+                    },
+                    take: 10,
+                });
+            });
+
+            it('should return a paging object with offset arguments when paging type is offset', () => {
+                process.env.PAGING_TYPE = 'offset';
+                const pagingArgs = service.addPaging(1, 10);
+                expect(pagingArgs).toEqual({
+                    skip: 0,
+                    take: 10,
+                });
+            });
         });
     });
 });
