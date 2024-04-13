734f953fd96dca41acc3d796d9b364acce8f6475
Markus Möller <markus.moeller@outlook.de>
2024-04-13 16:11:18 +0200

    refactor: docer and sentry

--- src/config/sentry.config.ts
+++ src/config/sentry.config.ts
@@ -1,14 +1,11 @@
-import { ProfilingIntegration } from '@sentry/profiling-node';
 import { NodeOptions } from '@sentry/node';
 
 const sentryConfig: NodeOptions = {
     dsn: process.env.SENTRY_DSN,
-    integrations: [new ProfilingIntegration()],
+    integrations: [],
     environment: process.env.NODE_ENV || 'development',
     release: `${process.env.npm_package_name}@${process.env.npm_package_version}`,
     tracesSampleRate: parseFloat(process.env.SENTRY_TRACES_SAMPLE_RATE) || 1.0,
-    profilesSampleRate:
-        parseFloat(process.env.SENTRY_PROFILES_SAMPLE_RATE) || 1.0,
     debug: process.env.NODE_ENV !== 'production',
 };
 
